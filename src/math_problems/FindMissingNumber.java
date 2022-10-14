db20798d18aaa3d139932219f764565c7b8b3332
madjidchebrine <mchebrine101@gmail.com>
2022-10-14 15:30:49 -0400

    short answer

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -8,6 +8,18 @@ public class FindMissingNumber {
 
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+int sum1 =0;
+
+        int a = 0;
+        for(int i = 0; i>=a.lenght; i++)
+        {
+            sum1=sum1+a[i];
+            System.out.println("Sum of elements in array:"+sum1);
+
+
+        }
 
     }
+
+
 }
