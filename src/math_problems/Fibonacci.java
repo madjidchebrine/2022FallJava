305b1c095b9ecc402fc302faf953de79e7a1351b
madjidchebrine <mchebrine101@gmail.com>
2022-10-13 21:14:01 -0400

    my lowest number

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -6,7 +6,7 @@ package math_problems;
 
     /** INSTRUCTIONS
      *
-     * Write a method that will pint or return at least 40 Fibonacci number
+     * Write a method that will print or return at least 40 Fibonacci number
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,7 +11,15 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+        int lowest = array [0];
+        int i = 0-25;            // 25 indexes in this array
 
+        for (i = 0 ; i <= 25; i++) {
+            if (  array [i] < lowest ) lowest = array [i] ;
+
+
+        }
+        System.out.println("the lowest number from this array is "+ lowest);
     }
 
 }
